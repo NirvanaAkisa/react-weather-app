b81ddaa5546887a0b4d533a8ea5f06aa0b2cfe10
NirvanaAkisa <137563355+NirvanaAkisa@users.noreply.github.com>
2023-11-08 23:07:31 +0200

    Improved my search engine

--- src/App.js
+++ src/App.js
@@ -5,21 +5,15 @@ import Weather from "./Weather.1";
 export default function App() {
   return (
     <div className="App">
-
       <div className="container">
-
-        <Weather />
-
+        <Weather defaultCity="Pretoria" />
       <footer> 
         This project was coded by Nirvana Sekoala and is {" "}
         <a href="https://github.com/NirvanaAkisa/react-weather-app" target="_blank" rel="norefferer">
           open-sourced on GitHub
         </a> 
         </footer>
-        
     </div>
-
-
     </div>
   );
 }
--- src/Weather.1.js
+++ src/Weather.1.js
@@ -1,11 +1,14 @@
 import React, {useState} from "react";
 import axios from "axios";
-import "./formattedDate";
+import "./WeatherInfo";
 import "./Weather.css";
+import WeatherInfo from "./WeatherInfo";
 
 
-export default function Weather() {
+export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ready: false});
+  
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
@@ -19,13 +22,28 @@ export default function Weather() {
     city: response.data.name
   });
 
-}
+  }
+  
+  function search() {
+    const apiKey = "1266ad07b66517497b1acf79ea5a6a64";
+    let city = "Pretoria";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  
+  }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -33,6 +51,7 @@ export default function Weather() {
                 placeholder="Search a city"
                 className="form-control"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -44,44 +63,14 @@ export default function Weather() {
             </div>
           </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <formattedDate date={weatherData.date} />
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <div className="d-flex weather-temperature">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <span className="temperature">{Math.round(weatherData.temperature)}</span>
-                <span className="unit">°C</span>
-              </div>
-            </div>
-          </div>
+        <WeatherInfo data={weatherData} />
 
-          <div className="col-6">
-            <ul>
-              <li>Humidity:{weatherData.humidity}%</li>
-              <li>Wind:{weatherData.wind}km/h</li>
-            </ul>
-          </div>
-        </div>
+       
       </div>
     );
   } else {
-     const apiKey = "1266ad07b66517497b1acf79ea5a6a64";
-     let city = "Pretoria";
-     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-    
+    search();
     return "Loading. . .";
- 
   }
-    
-    
+        
 }
