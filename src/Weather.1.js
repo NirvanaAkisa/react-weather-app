d0d2c10bd93db4a0450f6dbd24f4251805ae31c6
NirvanaAkisa <137563355+NirvanaAkisa@users.noreply.github.com>
2023-11-20 00:27:31 +0200

    Added animated real live data animated icons

--- src/Weather.1.js
+++ src/Weather.1.js
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
-import axios from "axios";
-import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherIcon from "./WeatherIcon";
+import "./Weather.css";
+import axios from "axios";
 
 export default function Weather(props) {
-  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
       wind: response.data.wind.speed,
-      description: response.data.weather[0].description,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      humidity: response.data.main.humidity,
       city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
     });
   }
 
@@ -29,6 +30,7 @@ export default function Weather(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+    search(city);
   }
 
   function handleCityChange(event) {
@@ -40,7 +42,7 @@ export default function Weather(props) {
       <div className="Weather">
         <form onSubmit={handleSubmit}>
           <div className="row">
-            <div className="col-9">
+            <div className="col-6">
               <input
                 type="search"
                 placeholder="Search a city"
@@ -49,12 +51,8 @@ export default function Weather(props) {
                 onChange={handleCityChange}
               />
             </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="search"
-                className="btn btn-primary w-100"
-              />
+            <div className="col-6">
+              <input type="submit" value="Search" className="btn btn-primary" />
             </div>
           </div>
         </form>
@@ -63,6 +61,6 @@ export default function Weather(props) {
     );
   } else {
     search();
-    return "Loading. . .";
+    return "Loading...";
   }
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,22 +1,22 @@
 import React from "react";
 import FormattedDate from "./FormattedDates";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul>
+      <ul> 
         <FormattedDate date={props.data.date} />
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex weather-temperature">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <div className="float-left">
+            <WeatherIcon code={props.data.icon} alt={props.data.description} />
+            
+              </div>
             <div className="float-left">
               <span className="temperature">
                 {Math.round(props.data.temperature)}
