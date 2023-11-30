9216acf6736d6bb7e06d08d12328d0c4995c4a92
NirvanaAkisa <137563355+NirvanaAkisa@users.noreply.github.com>
2023-11-30 13:42:48 +0200

    Created a forecast component with real live data

--- src/Weather.1.js
+++ src/Weather.1.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast"; 
 import "./Weather.css";
 import axios from "axios";
 
@@ -56,6 +57,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={ weatherData.coordinates} />
       </div>
     );
   } else {
